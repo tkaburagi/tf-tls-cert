6661249039c1d94cf4ce650e35a8e58c291c287d
tkaburagi <t.kaburagi@me.com>
2021-08-10 15:15:11 +0900

    first commit

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "tls_private_key" "ca" {
-  algorithm   = "${var.private_key_algorithm}"
+  algorithm   = var.private_key_algorithm
   ecdsa_curve = "${var.private_key_ecdsa_curve}"
   rsa_bits    = "${var.private_key_rsa_bits}"
 }
@@ -14,10 +14,10 @@ resource "tls_self_signed_cert" "ca" {
   is_ca_certificate = true
 
   validity_period_hours = "${var.validity_period_hours}"
-  allowed_uses          = ["${var.ca_allowed_uses}"]
+  allowed_uses          = "${var.ca_allowed_uses}"
 
   subject {
-    common_name  = "${var.ca_common_name}"
+    common_name  = var.ca_common_name
     organization = "${var.organization_name}"
   }
 
@@ -46,8 +46,8 @@ resource "tls_cert_request" "cert" {
   key_algorithm   = "${tls_private_key.cert.algorithm}"
   private_key_pem = "${tls_private_key.cert.private_key_pem}"
 
-  dns_names    = ["${var.dns_names}"]
-  ip_addresses = ["${var.ip_addresses}"]
+  dns_names    = "${var.dns_names}"
+  ip_addresses = "${var.ip_addresses}"
 
   subject {
     common_name  = "${var.common_name}"
@@ -63,7 +63,7 @@ resource "tls_locally_signed_cert" "cert" {
   ca_cert_pem        = "${tls_self_signed_cert.ca.cert_pem}"
 
   validity_period_hours = "${var.validity_period_hours}"
-  allowed_uses          = ["${var.allowed_uses}"]
+  allowed_uses          = "${var.allowed_uses}"
 
   # Store the certificate's public key in a file.
   provisioner "local-exec" {
--- vars.tf
+++ vars.tf
@@ -2,47 +2,44 @@
 # REQUIRED PARAMETERS
 # You must provide a value for each of these parameters.
 # ---------------------------------------------------------------------------------------------------------------------
+variable "ca_common_name" {
+  default = "vault.acme.io cert"
+}
 
 variable "ca_public_key_file_path" {
-  description = "Write the PEM-encoded CA certificate public key to this path (e.g. /etc/tls/ca.crt.pem)."
-}
-
-variable "public_key_file_path" {
-  description = "Write the PEM-encoded certificate public key to this path (e.g. /etc/tls/vault.crt.pem)."
-}
-
-variable "private_key_file_path" {
-  description = "Write the PEM-encoded certificate private key to this path (e.g. /etc/tls/vault.key.pem)."
-}
-
-variable "owner" {
-  description = "The OS user who should be given ownership over the certificate files."
-}
-
-variable "organization_name" {
-  description = "The name of the organization to associate with the certificates (e.g. Acme Co)."
-}
-
-variable "ca_common_name" {
-  description = "The common name to use in the subject of the CA certificate (e.g. acme.co cert)."
+  default = "/home/ubuntu/vault.crt.pem"
 }
 
 variable "common_name" {
-  description = "The common name to use in the subject of the certificate (e.g. acme.co cert)."
+  default = "acme.io cert"
 }
 
 variable "dns_names" {
-  description = "List of DNS names for which the certificate will be valid (e.g. vault.service.consul, foo.example.com)."
-  type        = "list"
+  default = ["acme.io"]
 }
 
 variable "ip_addresses" {
-  description = "List of IP addresses for which the certificate will be valid (e.g. 127.0.0.1)."
-  type        = "list"
+  default = ["10.10.1.57"]
+}
+
+variable "organization_name" {
+  default = "Acme Inc"
+}
+
+variable "private_key_file_path" {
+  default = "/home/ubuntu/vault.key.pem"
+}
+
+variable "public_key_file_path" {
+  default = "/home/ubuntu/vault.crt.pem"
 }
 
 variable "validity_period_hours" {
-  description = "The number of hours after initial issuing that the certificate will become invalid."
+  default = "765"
+}
+
+variable "owner" {
+  default = "ubuntu"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
